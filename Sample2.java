f2c5a3b44a861b98ced26b31667877ab479503d2
gunasekharvuyyuru <23bq1a54f4@vvit.net>
2026-07-08 13:37:28 +0530

    Organized Java programs into folders

--- Sample2.java
+++
@@ -1,8 +0,0 @@
-class Sample2 {
-    public static void main(String[] args){
-        int a = Integer.parseInt(args[0]);
-        int b = Integer.parseInt(args[1]);
-        int sum = a + b;
-        System.out.println("The sum of " + a  + " and  " + b + " is " + sum );
-    }
-}
--- javaFundamentals.java
+++
@@ -1,7 +0,0 @@
-class javaFundamentals{
-    public static void main (String[] args){
-        if(args.length == 2){
-            System.out.println(args[0] + " Technologies " + args[1]);
-        }
-    }
-}
