cbb9b8948dd04ed2711e51cff78de589346a78f8
gunasekharvuyyuru <23bq1a54f4@vvit.net>
2026-07-09 23:29:23 +0530

    Added Video Store mini project

---
+++ Miniproject opps and inheritance/VideoLauncher.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+public class VideoLauncher {
+    public static void main(String[] args) {
+        VideoStore store = new VideoStore();
+        Scanner scanner = new Scanner(System.in);
+        int choice = 0;
+
+        while (choice != 6) {
+            System.out.println("\nMAIN MENU");
+            System.out.println("=========");
+            System.out.println("1. Add Videos");
+            System.out.println("2. Check Out Video");
+            System.out.println("3. Return Video");
+            System.out.println("4. Receive Rating");
+            System.out.println("5. List Inventory");
+            System.out.println("6. Exit");
+            System.out.print("Enter your choice (1..6): ");
+
+            if (scanner.hasNextInt()) {
+                choice = scanner.nextInt();
+                scanner.nextLine();
+            } else {
+                System.out.println("Invalid input.");
+                scanner.nextLine();
+                continue;
+            }
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter the name of the video: ");
+                    String addName = scanner.nextLine();
+                    store.addVideo(addName);
+                    break;
+
+                case 2:
+                    System.out.print("Enter the name of the video: ");
+                    String checkoutName = scanner.nextLine();
+                    store.doCheckout(checkoutName);
+                    break;
+
+                case 3:
+                    System.out.print("Enter the name of the video: ");
+                    String returnName = scanner.nextLine();
+                    store.doReturn(returnName);
+                    break;
+
+                case 4:
+                    System.out.print("Enter the name of the video: ");
+                    String rateName = scanner.nextLine();
+                    System.out.print("Enter rating: ");
+                    int rating = scanner.nextInt();
+                    scanner.nextLine();
+                    store.receiveRating(rateName, rating);
+                    break;
+
+                case 5:
+                    store.listInventory();
+                    break;
+
+                case 6:
+                    System.out.println("Thank You!");
+                    break;
+
+                default:
+                    System.out.println("Invalid Choice.");
+            }
+        }
+
+        scanner.close();
+    }
+}
